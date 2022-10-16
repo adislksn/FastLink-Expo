bcace1a1e99a174f5512ac9a54542354185b402d
adislksn <adi.120140038@student.itera.ac.id>
2022-10-17 02:33:32 +0700

    Cloning src https://github.com/adislksn/Fast-Link

---
+++ src/database/index.js
@@ -0,0 +1,22 @@
+export const obj = [
+{
+    judul: "IG",
+    hyperlink: "https://www.instagram.com"
+},
+{
+    judul: "FB",
+    hyperlink: "https://www.facebook.com"
+},
+{
+    judul: "LinkedIn",
+    hyperlink: "https://www.linkedin.com"
+},
+{
+    judul: "Itera Adventure",
+    hyperlink: "https://ardhames.itch.io/itera-adventure-2"
+},
+{
+    judul: "Github",
+    hyperlink: "https://github.com/adislksn"
+},
+];
